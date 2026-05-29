e352f63eb32b59f28053916d4a5af747bb8c639b
336-afk <agussahaja336@gmail.com>
2026-05-29 21:36:12 +0700

    Update interface.cpp

--- boards/CYD-2432S028/interface.cpp
+++ boards/CYD-2432S028/interface.cpp
@@ -2,7 +2,6 @@
 #include "core/utils.h"
 #include <Arduino.h>
 #include <interface.h>
-#include <LittleFS.h>
 
 #if defined(HAS_CAPACITIVE_TOUCH)
 #if defined(TOUCH_GT911_I2C)
@@ -22,8 +21,8 @@ CYD28_TouchC touch(CYD28_DISPLAY_HOR_RES_MAX, CYD28_DISPLAY_VER_RES_MAX);
 #define XPT2046_CS TOUCH_CS
 #else
 #include "CYD28_TouchscreenR.h"
-#define CYD28_DISPLAY_HOR_RES_MAX 240
-#define CYD28_DISPLAY_VER_RES_MAX 320
+#define CYD28_DISPLAY_HOR_RES_MAX 320
+#define CYD28_DISPLAY_VER_RES_MAX 240
 CYD28_TouchR touch(CYD28_DISPLAY_HOR_RES_MAX, CYD28_DISPLAY_VER_RES_MAX);
 #if defined(TOUCH_XPT2046_SPI)
 #define XPT2046_CS XPT2046_SPI_CONFIG_CS_GPIO_NUM
@@ -32,12 +31,18 @@ CYD28_TouchR touch(CYD28_DISPLAY_HOR_RES_MAX, CYD28_DISPLAY_VER_RES_MAX);
 #endif
 #endif
 
+/***************************************************************************************
+** Function name: _setup_gpio()
+** Location: main.cpp
+** Description:   initial setup for the device
+***************************************************************************************/
 SPIClass touchSPI;
 void _setup_gpio() {
-#ifndef HAS_CAPACITIVE_TOUCH
+#ifndef HAS_CAPACITIVE_TOUCH // Capacitive Touchscreen uses I2C to communicate
     pinMode(XPT2046_CS, OUTPUT);
     digitalWrite(XPT2046_CS, HIGH);
 #endif
+
 #if defined(TOUCH_GT911_I2C)
     pinMode(BOARD_TOUCH_INT, INPUT);
     touch.setPins(-1, BOARD_TOUCH_INT);
@@ -45,27 +50,37 @@ void _setup_gpio() {
         Serial.println("Failed to find GT911 - check your wiring!");
     }
 #else
-#if !defined(USE_TFT_eSPI_TOUCH)
+#if !defined(USE_TFT_eSPI_TOUCH) // Use libraries
     if (!touch.begin()) {
         Serial.println("Touch IC not Started");
         log_i("Touch IC not Started");
     } else log_i("Touch IC Started");
 #endif
 #endif
+
     bruceConfig.colorInverted = 0;
 }
 
+/***************************************************************************************
+** Function name: _post_setup_gpio()
+** Location: main.cpp
+** Description:   second stage gpio setup to make a few functions work
+***************************************************************************************/
 void _post_setup_gpio() {
 #if defined(USE_TFT_eSPI_TOUCH)
     pinMode(TOUCH_CS, OUTPUT);
     uint16_t calData[5];
     File caldata = LittleFS.open("/calData", "r");
+
     if (!caldata) {
         tft.setRotation(ROTATION);
         tft.calibrateTouch(calData, TFT_WHITE, TFT_BLACK, 10);
+
         caldata = LittleFS.open("/calData", "w");
         if (caldata) {
-            caldata.printf("%d\n%d\n%d\n%d\n%d\n", calData[0], calData[1], calData[2], calData[3], calData[4]);
+            caldata.printf(
+                "%d\n%d\n%d\n%d\n%d\n", calData[0], calData[1], calData[2], calData[3], calData[4]
+            );
             caldata.close();
         }
     } else {
@@ -80,11 +95,18 @@ void _post_setup_gpio() {
     }
     tft.setTouch(calData);
 #endif
+
+    // Brightness control must be initialized after tft in this case @Pirata
     pinMode(TFT_BL, OUTPUT);
     ledcAttach(TFT_BL, TFT_BRIGHT_FREQ, TFT_BRIGHT_Bits);
     ledcWrite(TFT_BL, 255);
 }
 
+/*********************************************************************
+** Function: setBrightness
+** location: settings.cpp
+** set brightness value
+**********************************************************************/
 void _setBrightness(uint8_t brightval) {
     int dutyCycle;
     if (brightval == 100) dutyCycle = 255;
@@ -93,67 +115,133 @@ void _setBrightness(uint8_t brightval) {
     else if (brightval == 25) dutyCycle = 20;
     else if (brightval == 0) dutyCycle = 0;
     else dutyCycle = ((brightval * 255) / 100);
+
+    // log_i("dutyCycle for bright 0-255: %d", dutyCycle);
     ledcWrite(TFT_BL, dutyCycle);
 }
 
+/*********************************************************************
+** Function: InputHandler
+** Handles the variables PrevPress, NextPress, SelPress, AnyKeyPress and EscPress
+**********************************************************************/
 void InputHandler(void) {
     static long d_tmp = 0;
     if (millis() - d_tmp > 200 || LongPress) {
+        // I know R3CK.. I Should NOT nest if statements..
+        // but it is needed to not keep SPI bus used without need, it save resources
 #if defined(USE_TFT_eSPI_TOUCH)
         TouchPoint t;
         checkPowerSaveTime();
         bool _IH_touched = tft.getTouch(&t.x, &t.y);
         if (_IH_touched) {
-            NextPress = false; PrevPress = false; UpPress = false; DownPress = false;
-            SelPress = false; EscPress = false; AnyKeyPress = false;
-            NextPagePress = false; PrevPagePress = false;
-            touchPoint.pressed = false; _IH_touched = false;
+            NextPress = false;
+            PrevPress = false;
+            UpPress = false;
+            DownPress = false;
+            SelPress = false;
+            EscPress = false;
+            AnyKeyPress = false;
+            NextPagePress = false;
+            PrevPagePress = false;
+            touchPoint.pressed = false;
+            _IH_touched = false;
 #elif defined(TOUCH_GT911_I2C)
         static unsigned long tm = millis();
         TouchPointPro t;
         uint8_t touched = 0;
         uint8_t rot = 5;
+
         if (rot != bruceConfigPins.rotation) {
-            if (bruceConfigPins.rotation == 1) { touch.setMaxCoordinates(TFT_HEIGHT, TFT_WIDTH); touch.setSwapXY(true); touch.setMirrorXY(false, true); }
-            if (bruceConfigPins.rotation == 3) { touch.setMaxCoordinates(TFT_HEIGHT, TFT_WIDTH); touch.setSwapXY(true); touch.setMirrorXY(true, false); }
-            if (bruceConfigPins.rotation == 0) { touch.setMaxCoordinates(TFT_WIDTH, TFT_HEIGHT); touch.setSwapXY(false); touch.setMirrorXY(false, false); }
-            if (bruceConfigPins.rotation == 2) { touch.setMaxCoordinates(TFT_WIDTH, TFT_HEIGHT); touch.setSwapXY(false); touch.setMirrorXY(true, true); }
+            if (bruceConfigPins.rotation == 1) {
+                touch.setMaxCoordinates(TFT_HEIGHT, TFT_WIDTH);
+                touch.setSwapXY(true);
+                touch.setMirrorXY(false, true);
+            }
+            if (bruceConfigPins.rotation == 3) {
+                touch.setMaxCoordinates(TFT_HEIGHT, TFT_WIDTH);
+                touch.setSwapXY(true);
+                touch.setMirrorXY(true, false);
+            }
+            if (bruceConfigPins.rotation == 0) {
+                touch.setMaxCoordinates(TFT_WIDTH, TFT_HEIGHT);
+                touch.setSwapXY(false);
+                touch.setMirrorXY(false, false);
+            }
+            if (bruceConfigPins.rotation == 2) {
+                touch.setMaxCoordinates(TFT_WIDTH, TFT_HEIGHT);
+                touch.setSwapXY(false);
+                touch.setMirrorXY(true, true);
+            }
             rot = bruceConfigPins.rotation;
         }
+        // Track touch state to prevent double events on press/release
         static bool lastTouchState = false;
         static unsigned long lastTouchTime = 0;
+
         touched = touch.getPoint(&t.x, &t.y);
         bool currentTouchState = touched > 0;
-        if (currentTouchState && !lastTouchState && (millis() - lastTouchTime) > 100) { lastTouchTime = millis(); }
-        else if (!currentTouchState || lastTouchState) { touched = 0; }
+
+        // Only process new touch presses with debouncing
+        if (currentTouchState && !lastTouchState && (millis() - lastTouchTime) > 100) {
+            // This is a genuine new touch press
+            lastTouchTime = millis();
+        } else if (!currentTouchState || lastTouchState) {
+            // Touch release or continuing touch - ignore
+            touched = 0;
+        }
         lastTouchState = currentTouchState;
-        if (((millis() - tm) > 190 || LongPress) && touched) { tm = millis();
+        if (((millis() - tm) > 190 || LongPress) && touched) {
+            tm = millis();
 #else
         if (touch.touched()) {
             auto t = touch.getPointScaled();
 #endif
 #if !defined(TOUCH_GT911_I2C)
-            if (bruceConfigPins.rotation == 3) { t.y = (tftHeight + 20) - t.y; t.x = tftWidth - t.x; }
-            if (bruceConfigPins.rotation == 0) { int tmp = t.x; t.x = tftWidth - t.y; t.y = tmp; }
-            if (bruceConfigPins.rotation == 2) { int tmp = t.x; t.x = t.y; t.y = (tftHeight + 20) - tmp; }
+            // Serial.printf("\nRAW: Touch Pressed on x=%d, y=%d",t.x, t.y);
+            if (bruceConfigPins.rotation == 3) {
+                t.y = (tftHeight + 20) - t.y;
+                t.x = tftWidth - t.x;
+            }
+            if (bruceConfigPins.rotation == 0) {
+                int tmp = t.x;
+                t.x = tftWidth - t.y;
+                t.y = tmp;
+            }
+            if (bruceConfigPins.rotation == 2) {
+                int tmp = t.x;
+                t.x = t.y;
+                t.y = (tftHeight + 20) - tmp;
+            }
 #endif
+            // Serial.printf("\nROT: Touch Pressed on x=%d, y=%d\n", t.x, t.y);
+
             if (!wakeUpScreen()) AnyKeyPress = true;
             else goto END;
-            touchPoint.x = t.x; touchPoint.y = t.y; touchPoint.pressed = true;
+
+            // Touch point global variable
+            touchPoint.x = t.x;
+            touchPoint.y = t.y;
+            touchPoint.pressed = true;
             touchHeatMap(touchPoint);
         END:
             d_tmp = millis();
         }
     }
-    // Fallback tombol fisik
-    if (digitalRead(35) == LOW) { PrevPress = true; AnyKeyPress = true; delay(50); }
-    if (digitalRead(34) == LOW) { NextPress = true; AnyKeyPress = true; delay(50); }
-    if (digitalRead(0) == LOW)  { SelPress = true; AnyKeyPress = true; delay(50); }
 }
 
+/*********************************************************************
+** Function: powerOff
+** location: mykeyboard.cpp
+** Turns off the device (or try to)
+**********************************************************************/
 void powerOff() {
     esp_sleep_enable_ext0_wakeup(GPIO_NUM_0, LOW);
     esp_deep_sleep_start();
 }
 
+/*********************************************************************
+** Function: checkReboot
+** location: mykeyboard.cpp
+** Btn logic to turn off the device (name is odd btw)
+**********************************************************************/
 void checkReboot() {}
